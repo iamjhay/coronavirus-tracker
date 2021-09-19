6708004d7e46844efdcd4182c329f4213d87d8e2
john <johnnydrille7575@gmail.com>
2021-09-19 01:32:29 +0100

    Updates

---
+++ precache-manifest.4920d83bfc3ce83029b36b0821193e0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31e3bf12af933113bf77cbc873b73cc1",
+    "url": "/coronavirus-tracker/index.html"
+  },
+  {
+    "revision": "cabe3f2ff0e6c3134760",
+    "url": "/coronavirus-tracker/static/css/2.38ca0958.chunk.css"
+  },
+  {
+    "revision": "0595b8462117c3b0f4e6",
+    "url": "/coronavirus-tracker/static/css/main.555cce80.chunk.css"
+  },
+  {
+    "revision": "cabe3f2ff0e6c3134760",
+    "url": "/coronavirus-tracker/static/js/2.a164a772.chunk.js"
+  },
+  {
+    "revision": "2eec0a263ef1f7393322e8c89c5ff82a",
+    "url": "/coronavirus-tracker/static/js/2.a164a772.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0595b8462117c3b0f4e6",
+    "url": "/coronavirus-tracker/static/js/main.dd37d7ac.chunk.js"
+  },
+  {
+    "revision": "0fa8fd5f5d31cb2c824e",
+    "url": "/coronavirus-tracker/static/js/runtime-main.72c80331.js"
+  }
+]);
